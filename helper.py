36f70d4d7525c534af587dd07da24911c7e30cb1
leskat47 <lesliekate@gmail.com>
2018-04-17 18:51:12 -0700

    Move business logic to helper file

---
+++ helper.py
@@ -0,0 +1,80 @@
+import requests
+import os
+import json
+import time
+from datetime import datetime
+
+cache = {}
+DARK_SKY_KEY = os.environ["DARK_SKY_KEY"]
+
+class Commute(object):
+    """ A commute hour """
+
+    def __init__(self, hour, time, lat, lng):
+        self.time = time
+        self.hour = self.convert_hour(hour)
+        self.lat = lat
+        self.lng = lng
+
+        self.set_weather()
+
+    @staticmethod
+    def convert_hour(hour):
+        """ Convert from 24 hour to UNIX time"""
+
+        hour = datetime.today().replace(hour=hour, minute=0, second=0, microsecond=0)
+        hour = int(time.mktime(hour.timetuple()))
+
+        return hour
+
+    def get_hourly_weather(self):
+        """ Get today's weather info from Dark Sky API """
+
+        day = time.mktime(datetime.today().timetuple())
+
+        # check cache
+        if day in cache:
+            print "FOUND IN CACHE"
+            return cache[day]
+
+        # ping API for today's weather
+        url = "https://api.darksky.net/forecast/{key}/{lat},{lon},{time}".format(key=DARK_SKY_KEY,
+                                                                                 lat=self.lat,
+                                                                                 lon=self.lng,
+                                                                                 time=self.hour)
+        response = requests.get(url)
+        data = json.loads(response.text)
+        cache[day] = data["hourly"]["data"]
+        return data["hourly"]["data"]
+
+    def set_weather(self):
+        """ Set attributes for commute time weather """
+
+        for hour_weather in self.get_hourly_weather():
+            if hour_weather["time"] == self.hour:
+                self.wind = hour_weather["windSpeed"]
+                self.temperature = hour_weather["temperature"]
+                self.rain_probability = hour_weather["precipProbability"]
+                self.rain_intensity = hour_weather["precipIntensity"]
+                return
+        return
+
+    def recommendation(self):
+        print self.wind
+        if self.wind > 16:
+            return "Not a good ride: too windy!"
+        return "Looks good!"
+
+
+class MorningCommute(Commute):
+
+    def __init__(self, hour, lat, lng):
+
+        super(MorningCommute, self).__init__(hour, "morning", lat, lng)
+
+
+class EveningCommute(Commute):
+
+    def __init__(self, hour, lat, lng):
+
+        super(EveningCommute, self).__init__(hour + 12, "evening", lat, lng)
--- server.py
+++ server.py
@@ -1,19 +1,15 @@
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-from datetime import datetime
-import time
-import os
-import timezonefinder
-import requests
-import json
+from helper import MorningCommute, EveningCommute
+import redis
 
 # from model import connect_to_db, db
 
 
 app = Flask(__name__)
-
 app.secret_key = "ABC"
 
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.route('/')
 def index():
@@ -26,64 +22,29 @@ def get_weather():
 
     print request.args.get("lat")
     morning_hour = int(request.args.get("morning"))
-    evening_hour = int(request.args.get("evening")) + 12
+    evening_hour = int(request.args.get("evening"))
     latitude = float(request.args.get("lat"))
     longitude = float(request.args.get("lon"))
-    # import pdb; pdb.set_trace()
 
-    morning_weather = get_hour_weather(morning_hour, latitude, longitude)
-    evening_weather = get_hour_weather(evening_hour, latitude, longitude)
-    print "MORNING", morning_weather
+    # morning_weather = get_hour_weather(morning_hour, latitude, longitude)
+    # evening_weather = get_hour_weather(evening_hour, latitude, longitude)
+    #
+    # if not morning_weather or not evening_weather:
+    #     flash("Sorry no data was found")
+    #     redirect ("/")
+    #
+    # ride = {"morning": True, "evening": False}
+    #
+    # if morning_weather["windSpeed"] > 15:
+    #     report["morning"] = False
+    # if evening_weather["windSpeed"] > 15:
+    #     report["evening"] = False
 
-    if not morning_weather or not evening_weather:
-        flash("Sorry no data was found")
-        redirect ("/")
+    morning = MorningCommute(morning_hour, latitude, longitude)
+    evening = EveningCommute(evening_hour, latitude, longitude)
 
-    ride = {"morning": True, "evening": False}
+    return render_template("recommend.html", morning=morning, evening=evening)
 
-    if morning_weather["windSpeed"] > 15:
-        report["morning"] = False
-    if evening_weather["windSpeed"] > 15:
-        report["evening"] = False
-
-    return render_template("recommend.html", ride=ride)
-
-
-def get_hourly_weather(day, lat, lon, time):
-    """ Get today's weather info from Dark Sky API """
-
-    # check cache
-    # ping API for today's weather
-
-    key = os.environ["DARK_SKY_KEY"]
-
-    url = "https://api.darksky.net/forecast/{key}/{lat},{lon},{time}".format(key=key,
-                                                                             lat=lat,
-                                                                             lon=lon,
-                                                                             time=time)
-    print url
-    response = requests.get(url)
-    data = json.loads(response.text)
-
-
-    return data["hourly"]["data"]
-
-
-def get_hour_weather(hour, lat, lon):
-    day = time.mktime(datetime.today().timetuple())
-    hour = datetime.today().replace(hour=hour, minute=0, second=0, microsecond=0)
-    hour = int(time.mktime(hour.timetuple()))
-
-    # tf = timezonefinder.TimezoneFinder()
-    # tz_str = tf.certain_timezone_at(lat=lat, lng=lon)
-    hourly_weather = get_hourly_weather(hour,lat, lon, hour)
-    print "HOUR", hour
-    for obj in hourly_weather:
-        print obj["time"]
-        if obj["time"] == hour:
-            return obj
-
-    return None
 
 
 if __name__ == "__main__":
